2ce3611e73e129ae813dbad3b014fb04276afbfc
Roseokpe <roseokpe@gmail.com>
2022-10-20 15:57:45 -0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -36,8 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_20_003320) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "text"
-    t.integer "commentsCounter"
-    t.integer "likesCounter"
+    t.integer "comments_counter"
+    t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
@@ -47,7 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_20_003320) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "photo"
-    t.string "bio"
+    t.text "bio"
     t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
