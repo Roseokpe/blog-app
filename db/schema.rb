f90be9a345f37f24c10a6ac8a63a8c4f1104cfa9
Roseokpe <roseokpe@gmail.com>
2022-10-20 15:41:08 -0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -47,8 +47,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_20_003320) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "photo"
-    t.string "bIO"
-    t.integer "postsCounter"
+    t.string "bio"
+    t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
