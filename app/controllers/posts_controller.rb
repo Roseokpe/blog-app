a7698853bd0e5954eec6fff786e967afc0ed93d5
Roseokpe <roseokpe@gmail.com>
2022-10-29 11:35:32 -0700

    update the controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,22 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+  before_action :set_user, only: %i[index show]
+
   def index
     @posts = Post.all
   end
 
   def show
+    # @post = Post.find(params[:id])
+  end
+
+  private
+
+  def set_post
     @post = Post.find(params[:id])
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
   def index
-    # @users = User.all
-    @greetings = 'Hello World!'
+    @users = User.all
   end
 
   def show
+    # @user = User.find(params[:id])
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
   end
 end
