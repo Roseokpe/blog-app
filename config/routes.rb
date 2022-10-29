27e5b7a71f4b3c687242841b2a5e650515c2e87b
Roseokpe <roseokpe@gmail.com>
2022-10-29 14:18:23 -0700

    create form

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # root "articles#index"
   root 'users#index'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :likes, only: [:create] 
+      resources :comments, only: [:create, :new]
+    end
+   
   end
 end
