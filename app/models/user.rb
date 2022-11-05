56e05f56f04353af1b8450136422dd8ace7cd39c
adebowale <ademibowaleadegboye@gmail.com>
2022-11-05 22:34:38 +0100

    add authorisation for the users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+  
 
   has_many :posts, dependent: :destroy, foreign_key: :user_id
   has_many :comments, dependent: :destroy, foreign_key: :user_id
