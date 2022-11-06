1837d576a633f2af3f29d21781807de372b9b936
Roseokpe <roseokpe@gmail.com>
2022-11-05 21:21:36 -0700

    add devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,6 +25,22 @@ Rails.application.configure do
 
   # Enable server timing
   config.server_timing = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "roseokpe@gmail.com",
+    password: "rghgpcrinffiznjk", 
+    domain: "smtp.gmail.com",
+    openssl_verify_mode: "none",
+  }
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
